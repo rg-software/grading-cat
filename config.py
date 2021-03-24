3afbf526afb7c7660449bdeffecdf6411c4fc97d
Marina <none@none>
2021-03-24 10:12:10 +0300

    test2

---
+++ config.py
@@ -0,0 +1,18 @@
+##### diagram data #####
+FILE_NAME = ""
+
+STUDENTS_LIST = []
+RESULT_MATRIX = [[]]
+
+SELECTED_STUDENT = ""
+SELECTED_STUDENTS = []
+HIDED_STUDENTS = []
+
+##### check box #####
+SHOW_NAMES = True
+SHOW_LINKLESS = True
+SHOW_RARE = False
+CHESS = False
+
+##### slider #####
+RANGE_PLAG = 0
