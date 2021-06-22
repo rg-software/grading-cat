f0cb06eae5de85a9b9a574f84d12764e727afe72
Maxim Mozgovoy <rgsoftware@gmail.com>
2021-06-23 00:23:03 +0900

    moodle downloader major refactoring (in progress)

--- cli.py
+++ cli.py
@@ -7,30 +7,35 @@ import interop.moodle_downloader
 
 
 def jplag_preprocessor(config, assignment_name):
-	interop.jplag_preprocessor.preprocess_dirs(config["moodle_submissions_dir"], config["archive_dirs"], config['assignment_regex'], assignment_name)
+    interop.jplag_preprocessor.preprocess_dirs(
+        config["moodle_submissions_dir"],
+        config["archive_dirs"],
+        config["assignment_regex"],
+        assignment_name,
+    )
 
 
 def jplag_runner(config, assignment_name):
-	interop.jplag_runner.run(config['java_path'], config['jplag_args'], assignment_name)
+    interop.jplag_runner.run(config["java_path"], config["jplag_args"], assignment_name)
 
 
-def moodle_downloader(config):
-	interop.moodle_downloader.download(config)
+def moodle_downloader(config, _):
+    interop.moodle_downloader.download(config)
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		print("Commands:")
-		print("jplag_preprocessor <project-dir> <assignment-name>")
-		print("jplag_runner <project-dir> <assignment-name>")
-		print("moodle_downloader.py <project-dir>")
-		sys.exit(1)
+    if len(sys.argv) < 3:
+        print("Commands:")
+        print("jplag_preprocessor <project-dir> <assignment-name>")
+        print("jplag_runner <project-dir> <assignment-name>")
+        print("moodle_downloader <project-dir>")
+        sys.exit(1)
 
-	dir = os.getcwd()
-	os.chdir(sys.argv[2])					# project dir
-	with open('config.json') as f:
-		config = json.load(f)
+    dir = os.getcwd()
+    os.chdir(sys.argv[2])  # project dir
+    with open("config.json") as f:
+        config = json.load(f)
 
-	args = sys.argv[3:] + [""]				# remaining args
-	locals()[sys.argv[1]](config, args[0])
-	os.chdir(dir)
+    args = sys.argv[3:] + [""]  # remaining args
+    locals()[sys.argv[1]](config, args[0])
+    os.chdir(dir)
--- interop/moodle_downloader.py
+++ interop/moodle_downloader.py
@@ -1,110 +1,195 @@
-#https://docs.moodle.org/dev/Web_service_API_functions#Core_web_service_functions
+# https://docs.moodle.org/dev/Web_service_API_functions#Core_web_service_functions
 
-#The api documentation that provides more extensive documentation on not only what the required parameters are for all the available webservice functions but also the expected response and their structures in both REST and XML-RPC is accessible from the moodle site.
-# For access to these docs you must have admin access as they are found in admin submenu located at:
+# The api documentation that provides more extensive documentation on not only
+# what the required parameters are for all the available webservice functions
+# but also the expected response and their structures in both REST and XML-RPC
+# is accessible from the moodle site.
+# For access to these docs you must have admin access as they are found in
+# admin submenu located at:
 # site administration > Plugins > Web services > API Documentation
 
 import requests
 import os
-import sys
-import json
 import re
 import ast
+from dotmap import DotMap
 
 
-class StubProgressObject():
-	def setMaximum(self, _):
-		pass
-	
-	def setValue(self, _):
-		pass
+class StubProgressObject:  # for CLI (in GUI we use the real one)
+    def setMaximum(self, _):
+        pass
 
-	def processAppEvents(self, _):
-		pass
+    def setValue(self, _):
+        pass
 
-	def wasCanceled(self):
-		return False
+    def processAppEvents(self):
+        pass
+
+    def wasCanceled(self):
+        return False
 
 
-def regex_rename(conv_list, str):
-	for pattern, repl in conv_list:
-		if re.fullmatch(pattern, str):
-			return re.sub(pattern, repl, str)
-	return str
+def _regex_rename(conv_list, str):
+    for pattern, repl in conv_list:
+        if re.fullmatch(pattern, str):
+            return re.sub(pattern, repl, str)
+    return str
 
-def get_token(username, password, server_url):
-	response = requests.get(server_url + '/login/token.php', params={'username': username, 'password': password, 'service': 'moodle_mobile_app'})
-	return response.json()['token']
 
-def call_service(server_url, token, function, params):
-	params.update({'wstoken': token, 'wsfunction': function, 'moodlewsrestformat': 'json'})
-	response = requests.get(server_url + '/webservice/rest/server.php', params=params)
-	return response.json()
+class MoodleSession:
+    def __init__(self, cfg):
+        self.rest_url = f"{cfg.server_url}/webservice/rest/server.php"
+        self.login_url = f"{cfg.server_url}/login/token.php"
+        self.token = self._get_token(cfg.username, cfg.password)
+        self.user_dirs = self._user_dirs(cfg.course_shortname, cfg.username_conversions)
+        self.assignment_conversions = cfg.assignment_conversions
 
-def process_file(f, working_dir, token):
-	filename = f['filename']
-	download_url = f['fileurl'] + "?token=" + token
-	
-	if not os.path.exists(working_dir):
-		os.makedirs(working_dir)
+    def _user_dirs(self, course_shortname, conversions):
+        userdata = self._course_users(course_shortname)
+        users = {}  # id -> email-based username
+        for user in [DotMap(u) for u in userdata]:
+            conv = ast.literal_eval(conversions)  # TODO: invent something better
+            # '-' is bad for JPlag files
+            new_uname = _regex_rename(conv, user.email).replace("-", "_")
+            print(f"Renaming user: {user.email} -> {new_uname}")
+            users[user.id] = new_uname
+        return users
 
-	# TODO: checksum testing (?) -- no need to redownload already downloaded files
-	# TODO: perhaps, this script should 'sync' rather than download files: so we download new files and delete extra files
-	if not os.path.exists(os.path.join(working_dir, filename)):
-		print(f"\t\tDownloading file: {filename}")
-		r = requests.get(download_url)
-		if r.status_code == 200:
-			with open(os.path.join(working_dir, filename), 'wb') as f:
-				f.write(r.content)
+    def _call(self, function, p):
+        p.update(
+            {
+                "wstoken": self.token,
+                "wsfunction": function,
+                "moodlewsrestformat": "json",
+            }
+        )
+        r = requests.get(self.rest_url, params=p)
+        return r.json()
 
-def process_submission(submission, working_dir, users, token):
-	user_id = submission['userid']
-	user_email = users[user_id]
-	print(f"\tProcessing user: {user_email}")
-	
-	dir = os.path.join(working_dir, user_email)
+    def _get_token(self, username, password):
+        p = {
+            "username": username,
+            "password": password,
+            "service": "moodle_mobile_app",
+        }
+        r = requests.get(self.login_url, params=p)
+        return r.json()["token"]
 
-	for plugin in submission['plugins']:    # need to find attached files
-		if 'fileareas' in plugin:
-			areas = plugin['fileareas']     # perhaps, can be several areas, let's take the first one
-			for f in areas[0]['files']:
-				process_file(f, dir, token)
+    def _course_by_shortname(self, shortname):  # list of all matching courses
+        courses = self._call(
+            "core_course_get_courses_by_field",
+            {"field": "shortname", "value": shortname},
+        )
+        # we presume that only one course matches the given shortname
+        return courses["courses"][0]
 
-def process_assignment(server_url, assignment, working_dir, users, token, progressObject):
-	print(f"Saving assignment '{assignment['name']}' data") # such as "Exercises for Week 1"
-	submissions_full = call_service(server_url, token, 'mod_assign_get_submissions', {'assignmentids[0]': assignment['id']}) # matching submissions with ext info
-	submissions = submissions_full['assignments'][0]['submissions']  # submissions only
-	
-	for submission in submissions:
-		progressObject.processAppEvents()
-		if progressObject.wasCanceled():
-			raise RuntimeError()
-		process_submission(submission, os.path.join(working_dir, assignment['name']), users, token)
+    def userdir(self, user_id):
+        return self.user_dirs[user_id]
+
+    def _course_users(self, course_shortname):
+        print(course_shortname)
+        course_id = self._course_by_shortname(course_shortname)["id"]
+        r = self._call(
+            "core_enrol_get_enrolled_users",
+            {"courseid": course_id},
+        )
+        print(len(r))
+        return r
+
+    def course_assignments(self, course_shortname):
+        course_id = self._course_by_shortname(course_shortname)["id"]
+        # assignments with ext info
+        r = self._call(
+            "mod_assign_get_assignments",
+            {"courseids[0]": course_id},
+        )
+        return [Assignment(self, d) for d in r["courses"][0]["assignments"]]
+
+    def course_submissions(self, assignment_id):
+        # submissions with ext info
+        r = self._call(
+            "mod_assign_get_submissions",
+            {"assignmentids[0]": assignment_id},
+        )
+        return [Submission(self, d) for d in r["assignments"][0]["submissions"]]
+
+    def download_file(self, url, path):
+        r = requests.get(f"{url}?token={self.token}")
+        if r.status_code == 200:
+            with open(path, "wb") as f:
+                f.write(r.content)
+
+
+class SubmittedFile:
+    def __init__(self, session, dict):
+        self.session = session
+        self.file = DotMap(dict)
+
+    def process(self, working_dir):
+        if not os.path.exists(working_dir):
+            os.makedirs(working_dir)
+
+        # TODO: checksum testing (?) -- no need to redownload already downloaded files
+        # TODO: perhaps, this script should 'sync' rather than download files:
+        # so we download new files and delete extra files
+        target_path = os.path.join(working_dir, self.file.filename)
+        if not os.path.exists(target_path):
+            print(f"\t\tDownloading file: {self.file.filename}")
+            self.session.download_file(self.file.fileurl, target_path)
+
+
+class Submission:
+    def __init__(self, session, dict):
+        self.session = session
+        self.submission = DotMap(dict)
+
+    def process(self, working_dir):
+        user_dir = self.session.userdir(self.submission.userid)
+        print(f"\tProcessing user: {user_dir}")
+
+        full_dir = os.path.join(working_dir, user_dir)
+
+        for plugin in self.submission.plugins:  # need to find attached files
+            if "fileareas" in plugin:
+                # perhaps, can be several areas, let's take the first one
+                areas = plugin["fileareas"]
+                for f in areas[0]["files"]:
+                    SubmittedFile(self.session, f).process(full_dir)
+
+
+class Assignment:
+    def __init__(self, session, dict):
+        self.session = session
+        self.assignment = DotMap(dict)
+        conv = ast.literal_eval(session.assignment_conversions)
+        self.new_name = _regex_rename(conv, self.assignment.name)
+        print(f"Renaming asignment: {self.assignment.name} -> {self.new_name}")
+        # sel.assignment.name = new_aname
+
+    def process(self, working_dir, progressObject):
+        # such as "Exercises for Week 1"
+        print(f"Saving assignment '{self.assignment.name}' data")
+        submissions = self.session.course_submissions(self.assignment.id)
+
+        for submission in submissions:
+            progressObject.processAppEvents()
+            if progressObject.wasCanceled():
+                raise RuntimeError()
+            submission.process(os.path.join(working_dir, self.new_name))
+
 
 # NOTE: we should be inside the project dir here
-def download(config, progressObject=StubProgressObject()):
-	token = get_token(config['username'], config['password'], config['server_url'])
-	courses = call_service(config['server_url'], token, 'core_course_get_courses_by_field', {'field': 'shortname', 'value': config['course_shortname']}) # list of all matching courses
-	course_id = courses['courses'][0]['id'] # we presume that only one course matches the given shortname
-	users_data = call_service(config['server_url'], token, 'core_enrol_get_enrolled_users', {'courseid': course_id})
+def download(cfg, progressObject=StubProgressObject()):
+    cfg = DotMap(cfg)  # TODO: move DotMap() call to the outer module
+    s = MoodleSession(cfg)
 
-	users = {} # id -> email-based username
-	for user in users_data:
-		new_uname = regex_rename(ast.literal_eval(config['username_conversions']), user['email']).replace('-', '_') # '-' is bad for JPlag files
-		print(f"Renaming user: {user['email']} -> {new_uname}")
-		users[user['id']] = new_uname
+    assignments = s.course_assignments(cfg.course_shortname)
+    progressObject.setMaximum(len(assignments) - 1)
 
-	assignments_full = call_service(config['server_url'], token, 'mod_assign_get_assignments', params={'courseids[0]': course_id}) # matching assignments with ext info
-	assignments = assignments_full['courses'][0]['assignments'] # assignments only
+    try:
+        for i, assignment in enumerate(assignments):
+            progressObject.setValue(i)
+            assignment.process(cfg.moodle_submissions_dir, progressObject)
 
-	progressObject.setMaximum(len(assignments) - 1)
-	
-	try:
-		for i, assignment in enumerate(assignments):
-			progressObject.setValue(i)
-			new_aname = regex_rename(ast.literal_eval(config['assignment_conversions']), assignment['name'])
-			print(f"Renaming asignment: {assignment['name']} -> {new_aname}")
-			assignment['name'] = new_aname
-			process_assignment(config['server_url'], assignment, config['moodle_submissions_dir'], users, token, progressObject)
-	except RuntimeError:
-		print("Download process canceled")
+    except RuntimeError:
+        print("Download process canceled")
